2bd4a97228e5fc877720b96e05345d582fd1ad78
kvammee <elliott.kvamme@gmail.com>
2021-10-28 12:50:02 -0500

    Separated tests out to test each function individually

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,8 +6,28 @@ test("Create a new Employee", () => {
     expect(employee.name).toBe("Jack");
     expect(employee.id).toBe(1);
     expect(employee.email).toEqual("jack@gmail.com");
+})
+
+test("getName returns name", () => {
+    const employee = new Employee("Jack",1,"jack@gmail.com");
+
     expect(employee.getName()).toBe(employee.name);
+})
+
+test("getId returns employee id", () => {
+    const employee = new Employee("Jack",1,"jack@gmail.com");
+
     expect(employee.getId()).toBe(employee.id);
+})
+
+test("getEmail returns email address", () => {
+    const employee = new Employee("Jack",1,"jack@gmail.com");
+
     expect(employee.getEmail()).toBe(employee.email);
+})
+
+test("getRole returns 'Employee'", () => {
+    const employee = new Employee("Jack",1,"jack@gmail.com");
+
     expect(employee.getRole()).toBe("Employee");
 })
