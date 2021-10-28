e2dbd1f993e80ac6715fa78ac1112469e7f67492
kvammee <elliott.kvamme@gmail.com>
2021-10-28 12:44:55 -0500

    Created Employee object and constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,11 @@
 const Employee = require("../lib/Employee.js");
 
 test("Create a new Employee", () => {
-    const employee = new Employee("Jack");
+    const employee = new Employee("Jack",1,"jack@gmail.com");
 
     expect(employee.name).toBe("Jack");
-    expect(employee.id).toBe(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.id).toBe(1);
+    expect(employee.email).toEqual("jack@gmail.com");
     expect(employee.getName()).toBe(employee.name);
     expect(employee.getId()).toBe(employee.id);
     expect(employee.getEmail()).toBe(employee.email);
